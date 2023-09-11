910cdc0b4ebea3f3a323ceb13aae0cdb9f26a3e0
ingenierobeto25 <ingenierobeto25@gmail.com>
2023-09-11 10:51:22 -0400

    Slider OK, images mises sur html

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,3 +13,32 @@ function nav(){
 nav();
 
 });
+
+var image = $(".active img");
+var imageArray = [];
+var index = 0;
+
+
+image.each(function () {    
+	imageArray.push($(this).attr("src"));
+}); 
+
+function next(){
+	index=index+1;
+	if(index > imageArray.length-1){
+		index=0;
+	}
+	affiche();
+}
+
+function previous(){
+	index=index-1;
+	if(index <0){
+		index=imageArray.length-1;
+	}
+	affiche();
+}
+
+function affiche(){
+	$(".active img").attr("src",imageArray[index]);
+}
